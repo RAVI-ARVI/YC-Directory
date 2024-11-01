e2020ad4cba303e97811010ae18bd1134918c84c
Ravi <ravisankar@campx.in>
2024-11-01 14:28:13 +0530

    card added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -7,6 +8,19 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Ravi" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://st.depositphotos.com/1010652/3868/v/950/depositphotos_38686185-stock-illustration-cartoon-snack.jpg",
+      category: "Robots",
+      title: "We robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container ">
@@ -19,6 +33,22 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
